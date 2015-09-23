7580955c499a666392a4071c2a27fc39a5ea2f74
Matthew Kaufer <mjkaufer@gmail.com>
2015-09-23 11:16:29 -0400

    Style updates

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,13 @@
+var geometry = new THREE.SphereGeometry( 2, 10, 10 );
+var lineMaterial = new THREE.LineBasicMaterial({
+	color: 0x000000,
+	linewidth: 20
+});
+var material = new THREE.MeshBasicMaterial({
+	color: 0xff0000
+});
+
+
 function ballGroup(){//balls are at centers of group, and show where the group pivots
 	var group = new THREE.Object3D();
 
@@ -12,7 +22,7 @@ function lineTo(startVector, endVector){
 	var lineGeo = new THREE.Geometry()
 	lineGeo.vertices.push(startVector)
 	lineGeo.vertices.push(endVector)
-	return new THREE.Line(lineGeo, material)
+	return new THREE.Line(lineGeo, lineMaterial)
 }
 
 function xzVector(magnitude, angle){
