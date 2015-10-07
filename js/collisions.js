9e27b2008e4da42a8e0c6e65ad61148345790384
Matthew Kaufer <mjkaufer@gmail.com>
2015-10-07 14:34:07 -0400

    Whoops, moved wrong code

--- js/collisions.js
+++ js/collisions.js
@@ -1,61 +1,21 @@
+function lineIntersectsInfinitePlane(startPositionVector, endPositionVector, planeNormalVector, planePointVector){
 
-function vectorToGlobalVector(vector, matrixWorld){
-	return vector.clone().applyMatrix4(matrixWorld)
+	var displacementVector = endPositionVector.clone().sub(startPositionVector)
+	var denominator = planeNormalVector.dot(displacementVector)
+
+	if(denominator == 0)
+		return false;
+
+	var t = (planePointVector.dot(planeNormalVector) - planeNormalVector.dot(startPositionVector)) / denominator
+
+	return t >= 0 && t <= 1
 }
 
-function globalPosition(obj){
-	return vectorToGlobalVector(obj.position, obj.matrixWorld)
-}
+function pointInPlane(displacementVector, collisionTime, planeAVector, planeBVector, planeCVector){
+	var ABVector = planeBVector.clone().sub(planeAVector)
+	var BCVector = planeCVector.clone().sub(planeBVector)
+	var CAVector = planeAVector.clone().sub(planeCVector)
 
-function globalGeometry(obj){
-	var vertices = obj.geometry.vertices
-	var matrixWorld = obj.matrixWorld
-	var globalVertices = []
-
-	for(var i = 0; i < vertices.length; i++){
-		var vertex = vertices[i]
-		globalVertices.push(vectorToGlobalVector(vertex, matrixWorld))
-	}
-
-	return globalVertices
-
-}
-
-function isVector(object){
-	return THREE.Vector3.prototype.isPrototypeOf(object)
-}
-
-function getLineVectorPairs(object){
-	//traverses the object's children, looking for lines
-	//returns an array of position arrays
-	//e.g.
-	/*
-	[
-		[pointAStart, pointAEnd],
-		[pointBStart, pointBEnd]
-	]
-	*/
-
-
-
-	if(object.type == "Line")
-		return globalGeometry(object)
-
-	var vectorPairArray = []
-
-	for(var i = 0; i < object.children.length; i++){
-		
-		var potentialLineVectorPairs = getLineVectorPairs(object.children[i])
-
-		if(potentialLineVectorPairs.length > 0){
-			if(isVector(potentialLineVectorPairs[0]))
-				vectorPairArray.push(potentialLineVectorPairs)
-			else
-				vectorPairArray = vectorPairArray.concat(potentialLineVectorPairs)
-		}
-	}
-
-
-	return vectorPairArray
+	//unfinished, will implement later
 
 }
